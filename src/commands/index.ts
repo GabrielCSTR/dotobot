dd899a06abca003b9b5f68b6cceeeda779dc6ca1
Gabriel Carlos <gabrielcarlos91@hotmail.com>
2024-11-11 01:32:59 -0300

    feat: created new commands join rank and leave rank

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,5 +1,7 @@
+export { default as dotobot } from "./dotobot";
 export { default as join } from "./join";
 export { default as part } from "./part";
 export { default as meta } from "./meta";
 export { default as hero } from "./hero";
-export { default as dotobot } from "./dotobot";
+export { default as register } from "./register";
+export { default as leave } from "./leave";
